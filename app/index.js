cf80a78b3fb6f01e8cd42b694686c63748ba8465
James Moulang <jamesmoulang123@gmail.com>
2017-02-16 22:49:15 +0000

    production/staging

--- app/index.js
+++ app/index.js
@@ -5,42 +5,35 @@ const _s = require('underscore.string');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 // Ask James for this file. Put it in the same directory as index.js
-const token = require('./token');
+const token = process.env.TOKEN || require('./token');
+const production = process.env.NODE_ENV ? process.env.NODE_ENV === 'production' : false;
 
 bot.on('ready', () => {
-  console.log('My ultimate (receive discord server events) is fully charged!');
+  console.log('Running in ' + (production ? 'production ' : 'staging ') + 'mode.');
 });
 
 const MessageRoute = require('./MessageRoute');
 
 bot.on('message', message => {
-  // if (message.content === 'ping') {
-  //   message.channel.sendMessage('pong');
-  // }
-
-  // if (message.content.indexOf('!repeat ') > -1) {
-  // 	message.channel.sendMessage(message.content.replace('!repeat', ''));
-  // }
-
   const router = MessageRoute(
-  	/!.*/, 
-  	/!/,
-  	MessageRoute(
-		/repeat.*/, 
-		/repeat /,
-		(msg) => {
-			console.log(msg.content);
-			message.channel.sendMessage(msg.content);
-		}
-	),
-	MessageRoute(
-		/reverse.*/, 
-		/reverse /,
-		(msg) => {
-			console.log(msg.content);
-			message.channel.sendMessage(_s.reverse(msg.content));
-		}
-	)
+    production ? /!.*/ : /_.*/, 
+    production ? /!/ : /_/,
+    MessageRoute(
+      /repeat.*/, 
+      /repeat /,
+      (msg) => {
+        console.log(msg.content);
+        message.channel.sendMessage(msg.content);
+      }
+    ),
+    MessageRoute(
+      /reverse.*/, 
+      /reverse /,
+      (msg) => {
+        console.log(msg.content);
+        message.channel.sendMessage(_s.reverse(msg.content));
+      }
+    )
   );
   router(message);
 });
